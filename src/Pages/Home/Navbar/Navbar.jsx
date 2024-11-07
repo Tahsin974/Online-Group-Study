574d2a11a68af2a1ed75dc9533d8a61b122c5029
Tahsin974 <tahsinislam974@gmail.com>
2024-11-07 12:33:11 +0600

    update  navbar content

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -36,18 +36,25 @@ const Navbar = () => {
       onMenuOpenChange={setIsMenuOpen}
       className="bg-neutral py-3 items-center"
     >
-      <NavbarContent className="lg:hidden justify-start text-gray-200" >
+      <NavbarContent className=" justify-start text-gray-200" >
+        <div className="lg:hidden">
         <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-      </NavbarContent>
-
-      <NavbarContent className=" pr-3" justify="start">
-        <NavbarBrand >
+        </div>
+        <NavbarBrand className="lg:flex hidden" >
           
-          <img src={img} alt=""  className="lg:w-2/3 sm:w-3/5" />
+          <img src={img} alt="" className=" w-40" />
+        
         </NavbarBrand>
       </NavbarContent>
+      <NavbarBrand className="lg:hidden flex" >
+          
+          <img src={img} alt="" className=" w-40" />
+        
+        </NavbarBrand>
+
 
       <NavbarContent className="hidden lg:flex gap-4 text-lg font-semibold  justify-center" >
+        
         {menuItems}
       
       </NavbarContent>
@@ -60,17 +67,13 @@ const Navbar = () => {
         </NavbarItem>
         <NavbarItem>
           <Link>
-          <button className="btn bg-cyan-400  border-cyan-500 hover:bg-cyan-500 hover:border-cyan-500 text-white rounded-none">Register</button>
+          <button className="btn  bg-cyan-400  border-cyan-500 hover:bg-cyan-500 hover:border-cyan-500 text-white rounded-none">Register</button>
           </Link>
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarMenu className="bg-neutral text-lg font-semibold">
-        
+      <NavbarMenu className="bg-neutral  text-lg font-semibold py-6">
         {menuItems}
-        
-        
-        
       </NavbarMenu>
     </Menubar>
     </div>

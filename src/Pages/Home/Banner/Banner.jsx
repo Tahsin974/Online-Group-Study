e1934047cb3d4e5f33d64195475ec0111c7a035a
Tahsin974 <tahsinislam974@gmail.com>
2024-11-07 17:50:55 +0600

    adding footer and banner in home page

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -2,8 +2,8 @@
 import banner1 from '../../../assets/banner/banner1.jpg';
 import banner2 from '../../../assets/banner/banner2.jpg';
 import banner3 from '../../../assets/banner/banner3.jpg';
+
 import banner4 from '../../../assets/banner/banner4.jpg';
-import banner5 from '../../../assets/banner/banner5.jpg';
 
 
 
@@ -35,15 +35,15 @@ const Banner = () => {
         <SwiperSlide>
         <div className="relative">
           <img src={banner1} alt="" />
-          <div className="absolute top-0 left-0 lg:pl-28 md:pl-14 pl-8 pr-2 lg:w-1/2 md:w-1/2 sm:w-2/3 w-full h-full bg-gradient-to-r from-[#151515] to-[#15151500] text-white flex items-center">
-            <div className="lg:space-y-7 space-y-1" >
+          <div className="absolute top-0 left-0    w-full h-full bg-black bg-opacity-65  flex items-center text-center justify-center">
+            <div className="lg:space-y-7 space-y-2 text-white lg:w-3/4" >
             <h1 className=" lg:text-5xl md:text-3xl text-lg font-bold">
             Learn, Share, Succeed with Study Buddies
             </h1>
             <p>
             <q>Unlock the power of collaboration with Study Buddies. Connect with peers from around the globe, share knowledge.Our platform is designed to make learning interactive and fun.</q>
             </p>
-            <div className="flex space-x-3">
+            <div className="flex justify-center space-x-3">
               <button className="btn lg:btn-md md:btn-md btn-sm btn-outline  border-cyan-500 hover:bg-cyan-500 hover:border-cyan-500 text-cyan-400  hover:text-white ">
               Sign Up for Free
               </button>
@@ -55,15 +55,15 @@ const Banner = () => {
       <SwiperSlide>
       <div className="relative">
           <img src={banner2} alt="" />
-          <div className="absolute top-0 left-0 lg:pl-28 md:pl-14 pl-8 pr-2 lg:w-1/2 md:w-1/2 sm:w-2/3 w-full h-full bg-gradient-to-r from-[#151515] to-[#15151500] text-white flex items-center">
-            <div className="lg:space-y-7 space-y-1" >
+          <div className="absolute top-0 left-0    w-full h-full bg-black bg-opacity-65  flex items-center text-center justify-center">
+            <div className="lg:space-y-7 space-y-2 text-white lg:w-3/4" >
             <h1 className=" lg:text-5xl md:text-3xl text-lg font-bold">
             Learn, Share, Succeed with Study Buddies
             </h1>
             <p>
             <q>Unlock the power of collaboration with Study Buddies. Connect with peers from around the globe, share knowledge.Our platform is designed to make learning interactive and fun.</q>
             </p>
-            <div className="flex space-x-3">
+            <div className="flex justify-center space-x-3">
               <button className="btn lg:btn-md md:btn-md btn-sm btn-outline  border-cyan-500 hover:bg-cyan-500 hover:border-cyan-500 text-cyan-400  hover:text-white ">
               Sign Up for Free
               </button>
@@ -75,15 +75,15 @@ const Banner = () => {
       <SwiperSlide>
       <div className="relative">
           <img src={banner3} alt="" />
-          <div className="absolute top-0 left-0 lg:pl-28 md:pl-14 pl-8 pr-2 lg:w-1/2 md:w-1/2 sm:w-2/3 w-full h-full bg-gradient-to-r from-[#151515] to-[#15151500] text-white flex items-center">
-            <div className="lg:space-y-7 space-y-1" >
+          <div className="absolute top-0 left-0    w-full h-full bg-black bg-opacity-65  flex items-center text-center justify-center">
+            <div className="lg:space-y-7 space-y-2 text-white lg:w-3/4" >
             <h1 className=" lg:text-5xl md:text-3xl text-lg font-bold">
             Learn, Share, Succeed with Study Buddies
             </h1>
             <p>
             <q>Unlock the power of collaboration with Study Buddies. Connect with peers from around the globe, share knowledge.Our platform is designed to make learning interactive and fun.</q>
             </p>
-            <div className="flex space-x-3">
+            <div className="flex justify-center space-x-3">
               <button className="btn lg:btn-md md:btn-md btn-sm btn-outline  border-cyan-500 hover:bg-cyan-500 hover:border-cyan-500 text-cyan-400  hover:text-white ">
               Sign Up for Free
               </button>
@@ -92,38 +92,19 @@ const Banner = () => {
           </div>
         </div>
       </SwiperSlide>
+      
       <SwiperSlide>
       <div className="relative">
           <img src={banner4} alt="" />
-          <div className="absolute top-0 left-0 lg:pl-28 md:pl-14 pl-8 pr-2 lg:w-1/2 md:w-1/2 sm:w-2/3 w-full h-full bg-gradient-to-r from-[#151515] to-[#15151500] text-white flex items-center">
-            <div className="lg:space-y-7 space-y-1" >
+          <div className="absolute top-0 left-0    w-full h-full bg-black bg-opacity-65  flex items-center text-center justify-center">
+            <div className="lg:space-y-7 space-y-2 text-white lg:w-3/4" >
             <h1 className=" lg:text-5xl md:text-3xl text-lg font-bold">
             Learn, Share, Succeed with Study Buddies
             </h1>
             <p>
             <q>Unlock the power of collaboration with Study Buddies. Connect with peers from around the globe, share knowledge.Our platform is designed to make learning interactive and fun.</q>
             </p>
-            <div className="flex space-x-3">
-              <button className="btn lg:btn-md md:btn-md btn-sm btn-outline  border-cyan-500 hover:bg-cyan-500 hover:border-cyan-500 text-cyan-400  hover:text-white ">
-              Sign Up for Free
-              </button>
-            </div>
-            </div>
-          </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-      <div className="relative">
-          <img src={banner5} alt="" />
-          <div className="absolute top-0 left-0 lg:pl-28 md:pl-14 pl-8 pr-2 lg:w-1/2 md:w-1/2 sm:w-2/3 w-full h-full bg-gradient-to-r from-[#151515] to-[#15151500] text-white flex items-center">
-            <div className="lg:space-y-7 space-y-1" >
-            <h1 className=" lg:text-5xl md:text-3xl text-lg font-bold">
-            Learn, Share, Succeed with Study Buddies
-            </h1>
-            <p>
-            <q>Unlock the power of collaboration with Study Buddies. Connect with peers from around the globe, share knowledge.Our platform is designed to make learning interactive and fun.</q>
-            </p>
-            <div className="flex space-x-3">
+            <div className="flex justify-center space-x-3">
               <button className="btn lg:btn-md md:btn-md btn-sm btn-outline  border-cyan-500 hover:bg-cyan-500 hover:border-cyan-500 text-cyan-400  hover:text-white ">
               Sign Up for Free
               </button>
--- src/Pages/Home/Footer/Footer.jsx
+++ src/Pages/Home/Footer/Footer.jsx
@@ -1,10 +1,23 @@
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const navItems = (
+    <>
+      <Link className="text-gray-200 cursor-pointer">Home</Link>
+      <Link className="text-gray-200 cursor-pointer">Assignments</Link>
+    </>
+  );
+  return (
+    <div className="min-w-screen">
+      <footer className="footer  footer-center bg-neutral text-neutral-content items-center border-neutral p-4">
+        <aside className="grid-flow-col items-center">
+          
+          <p>Copyright © {new Date().getFullYear()} - All right reserved by Online Study</p>
+        </aside>
+        
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
